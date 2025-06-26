c5f3fc022cb224ea068cef0d3065fa8697e543bd
Aditya kumar singh <aditya.21scse1280002@galgotiasuniversity.edu.in>
2025-06-26 18:16:02 +0530

    port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/token', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`ACS Token Server running on http://localhost:${PORT}`);
 });
